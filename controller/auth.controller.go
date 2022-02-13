996fb27e73a8fd918adb2cd301e4c181623d94ea
mehmethalis <mehmethaliscicek@gmail.com>
2022-02-13 18:30:50 +0300

    add token detail in response

--- controller/auth.controller.go
+++ controller/auth.controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"haliscicek.com/gin-api/dto"
 	"haliscicek.com/gin-api/helper"
@@ -38,7 +39,18 @@ func (c *authController) Login(ctx *gin.Context) {
 	authResult := c.authService.VerifyCredential(loginDTO.Email, loginDTO.Password)
 	if v, ok := authResult.(model.User); ok {
 		generatedToken := c.jwtService.GenerateToken(strconv.FormatUint(v.ID, 10))
+		tokenDetail, err := c.jwtService.ValidateToken(generatedToken)
+		if err != nil {
+			response := helper.BuildErrorResponse("Token can not validate", err.Error(), helper.EmptyObject{})
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, response)
+			return
+		}
+		exp := tokenDetail.Claims.(jwt.MapClaims)["exp"].(float64)
+		iat := tokenDetail.Claims.(jwt.MapClaims)["iat"].(float64)
+
 		v.Token = generatedToken
+		v.ExpiresAt = exp
+		v.IssuedAt = iat
 		response := helper.BuildResponse(true, "OK!", v)
 		ctx.JSON(http.StatusOK, response)
 		return
@@ -61,6 +73,17 @@ func (c *authController) Register(ctx *gin.Context) {
 	} else {
 		createdUser := c.authService.CreateUser(registerDTO)
 		token := c.jwtService.GenerateToken(strconv.FormatUint(createdUser.ID, 10))
+		tokenDetail, err := c.jwtService.ValidateToken(token)
+		if err != nil {
+			response := helper.BuildErrorResponse("Token can not validate", err.Error(), helper.EmptyObject{})
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, response)
+			return
+		}
+		exp := tokenDetail.Claims.(jwt.MapClaims)["exp"].(float64)
+		iat := tokenDetail.Claims.(jwt.MapClaims)["iat"].(float64)
+
+		createdUser.ExpiresAt = exp
+		createdUser.IssuedAt = iat
 		createdUser.Token = token
 		response := helper.BuildResponse(true, "OK!", createdUser)
 		ctx.JSON(http.StatusCreated, response)
--- service/jwt.service.go
+++ service/jwt.service.go
@@ -41,7 +41,7 @@ func (j *jwtService) GenerateToken(UserID string) string {
 	claims := &jwtCustomClaim{
 		UserID,
 		jwt.StandardClaims{
-			ExpiresAt: time.Now().AddDate(1, 0, 0).Unix(),
+			ExpiresAt: time.Now().Add(time.Minute * 40).Unix(),
 			Issuer:    j.issuer,
 			IssuedAt:  time.Now().Unix(),
 		},
